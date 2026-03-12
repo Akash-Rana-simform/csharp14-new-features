b1217e9cffa2da855669caee151a7b5c39e56c70
Akash-Sabre <arana@sabreindustries.com>
2026-03-12 17:45:06 +0530

    Add C# 14 feature demos and project setup
    Introduced new CSharp14Demo project targeting .NET 10.0 with preview language features enabled. Added demo files for extension members, field keyword, null-conditional assignment, and partial members. Updated Program.cs to run all demos. Created solution and project files for organization.

---
+++ CSharp14Demo/PartialMembersDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CSharp14Demo
+{
+    public static class PartialMembersDemo
+    {
+        public static void Run()
+        {
+            var demo = new Demo();
+
+            demo.TestEvent += () => Console.WriteLine("Event fired");
+
+            demo.RaiseEvent();
+        }
+    }
+
+    public partial class Demo
+    {
+        // Public constructor
+        public Demo()
+        {
+            Initialize();
+        }
+
+        // C# 14 Partial Constructor Declaration
+        partial void Initialize();
+
+        // C# 14 Partial Event Declaration
+        public partial event Action TestEvent;
+
+        public void RaiseEvent()
+        {
+            _testEvent?.Invoke();
+        }
+    }
+
+    public partial class Demo
+    {
+        // Partial Constructor-like Implementation
+        partial void Initialize()
+        {
+            Console.WriteLine("Initialization done");
+        }
+
+        private Action _testEvent;
+
+        // C# 14 Partial Event Implementation
+        public partial event Action TestEvent
+        {
+            add => _testEvent += value;
+            remove => _testEvent -= value;
+        }
+    }
+}
